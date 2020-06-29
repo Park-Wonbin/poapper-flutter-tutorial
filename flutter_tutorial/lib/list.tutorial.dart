9ace1b17a2c6651d5be0252954d4808f7e2d9536
Park-Wonbin <wonbin9413@postech.ac.kr>
2020-06-30 05:38:27 +0900

    updated lecture 02

--- flutter_tutorial/lib/list.tutorial.dart
+++ flutter_tutorial/lib/list.tutorial.dart
@@ -4,7 +4,41 @@ class ListTutorial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final title = 'Basic List';
+    
+    List<ListItem> items = List<ListItem>.generate(
+      50,
+      (i) => i % 6 == 0
+      ? HeadingItem("Heading $i")
+          : MessageItem("Sender $i", "Message body $i")
+      );
 
+    // Sample3
+    Widget customListView = ListView.builder(
+      // Let the ListView know how many items it needs to build.
+      itemCount: items.length,
+      // Provide a builder function. This is where the magic happens.
+      // Convert each item into a widget based on the type of item it is.
+      // ignore: missing_return
+      itemBuilder: (context, index) {
+        final item = items[index];
+
+        if (item is HeadingItem) {
+          return ListTile(
+            title: Text(
+              item.heading,
+              style: Theme.of(context).textTheme.headline,
+            ),
+          );
+        } else if (item is MessageItem) {
+          return ListTile(
+            title: Text(item.sender),
+            subtitle: Text(item.body),
+          );
+        }
+      },
+    );
+
+    // Sample1
     Widget myGridView = GridView.count(
       // Create a grid with 2 columns. If you change the scrollDirection to
       // horizontal, this produces 2 rows.
@@ -20,6 +54,7 @@ class ListTutorial extends StatelessWidget {
       }),
     );
 
+    // Sample2
     Widget myListView = ListView(
       children: <Widget>[
         ListTile(
@@ -37,12 +72,30 @@ class ListTutorial extends StatelessWidget {
       ],
     );
 
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: myListView,
+      body: customListView,
     );
   }
+}
 
+// The base class for the different types of items the list can contain.
+abstract class ListItem {}
+
+// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+final String heading;
+
+HeadingItem(this.heading);
+}
+
+// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+final String sender;
+final String body;
+
+MessageItem(this.sender, this.body);
 }
